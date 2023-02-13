cc8926ca92d2e297097759afb7d8d90da20ade05
andrercastillon <andrercastillon@gmail.com>
2023-02-12 21:04:54 -0600

    validate.js init

---
+++ readme.md
@@ -0,0 +1,5 @@
+1. Problem needs to be solved = sending form information to another .html page using js (having at least 1 event listener)
+
+2. a. Store information via submit by setting local storage 
+   b. Rediricting user to result.html
+   c. Get information from local to display it
---
+++ validate.js
@@ -0,0 +1,17 @@
+function validate() {
+  console.log("!")
+  // const form = document.getElementById('form');
+//   const name = document.getElementsById('name');
+//   const email = document.getElementsById'email');
+
+//   form.addEventListener('submit', function(e) {
+//     e.preventDefault();
+
+//     const nameValue = name.value;
+//     const emailValue = email.value;
+
+//     localStorage.setItem('name', nameValue);
+//     localStorage.setItem('email', emailValue)
+
+//   })
+}
